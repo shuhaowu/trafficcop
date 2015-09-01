70f51b4be6d77a2338b19eabf2f5c3f4300e9501
Shuhao Wu <shuhao@shuhaowu.com>
2015-09-01 10:39:25 -0400

    Better package behaviour

--- build.sh
+++ build.sh
@@ -11,12 +11,6 @@ set -e
 devices=$(ls -d devices/*/)
 
 for device in $devices; do
-  rm -rf $device/files/trafficcop
-  mkdir -p $device/files/trafficcop
-
-  cp -r src/api $device/files/trafficcop
-  cp src/index.html $device/files/trafficcop/index.html
-
   if [ ! -e $device/files/etc/dropbear/authorized_keys ]; then
     echo "ERROR: need authorized_keys for $device"
     exit 1
